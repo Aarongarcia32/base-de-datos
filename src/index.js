69342b1a6e69d523d6f1449e27c9abbda12cf897
Aarongarcia32 <2402055@utrivieramaya.edu.mx>
2025-12-15 13:13:51 -0600

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const config = require('./config');
-
-const PORT = config.app.port;
-const HOST = config.app.host;
-
-app.listen(app.get('port'), () => {
-   console.log("servidor escuchando en el puerto ", app.get("port"));
-});
